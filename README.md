c1dd4af32c9cc4df3947397aa07daacfcea54606
pythias <pythias@gmail.com>
2020-03-02 12:46:22 +0800

    使用fetch

--- README.md
+++ README.md
@@ -4,8 +4,16 @@
 
 ## 使用方法
 
-1. 使用 chrome 打开 https://weibo.com 并登录 (https)
-2. 打开调试窗口，复制 [weibo-start-over.js](https://raw.githubusercontent.com/pythias/weibo-start-over/master/weibo-start-over.js) 中的全部代码， 在 console 中粘贴代码后回车
-3. 请在console输入 start() 
+1. 使用 chrome 打开 https://weibo.com 并登录
+2. 打开调试窗口，复制以下代码至console，执行既可
+```js
+fetch("https://raw.githubusercontent.com/pythias/weibo-start-over/master/weibo-start-over.js")
+    .then(response => response.text())
+    .then(text => {
+        eval(text);
+        cleanup.start();
+    });
+```
+
 
 
--- weibo-start-over.js
+++ weibo-start-over.js
@@ -1,91 +1,89 @@
-// 1. 使用 chrome 打开 weibo.com （确保你登录了微博）
-// 2. 打开调试窗口，在 console 中贴下面的代码后回车
-// 3. 如需删除其他微博，请输入一下内容后回车：start(); 
+var cleanup = {
+    mids: [],
+    midIndex: 0,
+    timer: null,
+    running: false,
 
-var mids = [];
-var midIndex = 0;
-var timer = null;
-var running = false;
-var http = new XMLHttpRequest();
+    cleanNextPage: function() {
+        let url = 'https://weibo.com/p/' + $CONFIG['page_id'] + '/home';
+        http = new XMLHttpRequest();
+        http.open('GET', url, true);
+        http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        http.send();
+        http.onreadystatechange = function() {
+            if (http.readyState != 4 || http.status != 200) {
+                return;
+            }
 
-function cleanNextPage() {
-    let url = 'https://weibo.com/p/' + $CONFIG['page_id'] + '/home';
-    http.open('GET', url, true);
-    http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    http.send();
-    http.onreadystatechange = function() {
-        if (http.readyState != 4 || http.status != 200) {
+            let matches = http.responseText.match(/mid=([0-9]+)/g);
+            if (matches == null) {
+                cleanup.stop("恭喜你！\n如有漏网，请再执行一遍");
+                return;
+            }
+            
+            let values = {};
+            matches.forEach(function(match) {
+                values[match.substr(4)] = 0;
+            }, this);
+
+            cleanup.mids = Object.keys(values);
+            cleanup.timer = setInterval('cleanup.deleteNextWeibo();', 1000);
+
+            console.log('即将清理 ' + cleanup.mids.length + ' 条微博');
+        }
+    },
+
+    deleteNextWeibo: function() {
+        if (cleanup.midIndex >= cleanup.mids.length) {
+            cleanup.mids = [];
+            cleanup.midIndex = 0;
+            clearInterval(cleanup.timer);
+            setTimeout('cleanup.cleanNextPage();', 1000);
             return;
         }
 
-        let matches = http.responseText.match(/mid=([0-9]+)/g);
-        if (matches == null) {
-            stop('恭喜你，可以重新来过了。如果还有请再执行一遍 ');
-            return;
+        cleanup.deleteWeibo(cleanup.mids[cleanup.midIndex]);
+        cleanup.midIndex++;
+    },
+
+    deleteWeibo: function(mid) {
+        http = new XMLHttpRequest();
+        http.open('POST', 'https://weibo.com/aj/mblog/del?ajwvr=6', true);
+        http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        http.send('mid=' + mid);
+        http.onreadystatechange = function() {
+            if (http.readyState != 4 || http.status != 200) {
+                return;
+            }
+
+            try {
+                const json = JSON.parse(http.responseText);
+                if (json.code == 100000) {
+                    console.log('清理 [' + mid + ']');
+                }
+            } catch (error) {
+                return;
+            }
         }
-        
-        let values = {};
-        matches.forEach(function(match) {
-            values[match.substr(4)] = 0;
-        }, this);
+    },
 
-        mids = Object.keys(values);
-        timer = setInterval('deleteNextWeibo();', 1000);
+    stop: function(message) {
+        clearInterval(cleanup.timer);
+        cleanup.running = false;
+        console.log(message);
+    },
 
-        console.log('本页有 ' + mids.length + ' 条微博');
-    }
-}
-
-function deleteNextWeibo() {
-    if (midIndex >= mids.length) {
-        mids = [];
-        midIndex = 0;
-        clearInterval(timer);
-        setTimeout('cleanNextPage();', 1000);
-        return;
-    }
-
-    deleteWeibo(mids[midIndex]);
-    midIndex++;
-}
-
-function deleteWeibo(mid) {
-    http.open('POST', 'https://weibo.com/aj/mblog/del?ajwvr=6', true);
-    http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    http.send('mid=' + mid);
-    http.onreadystatechange = function() {
-        if (http.readyState != 4 || http.status != 200) {
+    start: function() {
+        if (cleanup.running) {
+            console.log('正在进行中, 请稍后或者刷新页面后再执行.');
             return;
         }
 
-        let json = {}
-        try {
-            json = JSON.parse(http.responseText);
-        } catch (error) {
-            return;
-        }
-
-        if (json.code == 100000) {
-            console.log('删除成功 - ' + mid);
-        }
-    }
+        console.log("微博重头来过v1.0.0\n开始执行");
+        cleanup.running = true;
+        cleanup.mids = [];
+        cleanup.midIndex = 0;
+        cleanup.cleanNextPage();
+    },
 }
 
-function stop(message) {
-    clearInterval(timer);
-    running = false;
-    console.log(message);
-}
-
-function start() {
-    if (running) {
-        console.log('进行中...');
-        return;
-    }
-
-    console.log('开始删除');
-    running = true;
-    mids = [];
-    midIndex = 0;
-    cleanNextPage();
-}
